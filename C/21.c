d86f9bb59d7892fa898a3810b047d7d0547ba01c
fabiooo4 <fabioirimie@gmail.com>
2023-11-26 11:30:42 +0100

    updated 21.c

--- C/21.c
+++ C/21.c
@@ -2,45 +2,112 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct ListNode {
+typedef struct ListNode {
   int val;
   struct ListNode *next;
-};
+} ListNode;
 
-bool addAfterNode(struct ListNode *list, int val) {
-  struct ListNode *newNode = (struct ListNode *)malloc(sizeof(struct ListNode));
-  if (newNode == NULL) {
-    return false;
+void printList(ListNode *head) {
+  ListNode *tmp = head;
+
+  while (tmp != NULL) {
+    printf("%d ", tmp->val);
+    tmp = tmp->next;
   }
-  newNode->val = val;
-  newNode->next = list->next;
-  list->next = newNode;
-
-  return 1;
 }
 
-void printList(struct ListNode *list) {
-  struct ListNode *temp = list;
-  printf("[ ");
-  while (temp != NULL) {
-    printf("%d ", temp->val);
-    temp = temp->next;
+void appendNode(ListNode **head, int n) {
+  ListNode *newNode = malloc(sizeof(ListNode));
+  ListNode *last = *head;
+
+  if (newNode == NULL)
+    return;
+
+  newNode->val = n;
+  newNode->next = NULL;
+
+  if (*head == NULL) {
+    *head = newNode;
+    return;
   }
-  printf("]");
-  printf("\n");
+
+  while (last->next != NULL) {
+    last = last->next;
+  }
+
+  last->next = newNode;
+  return;
 }
 
-struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2) {
-  struct ListNode *output;
+ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+  ListNode *output = NULL;
+
+  if (list1 == NULL && list2 == NULL) {
+    return NULL;
+  }
+
+  if (list1 == NULL) {
+    return list2;
+  }
+
+  if (list2 == NULL) {
+    return list1;
+  }
+
+  while (list1 != NULL && list2 != NULL) {
+    printf("%d < %d\n", list1->val, list2->val);
+    if (list1->val < list2->val) {
+      appendNode(&output, list1->val);
+      list1 = list1->next;
+    } else if (list1->val == list2->val) {
+      appendNode(&output, list1->val);
+      appendNode(&output, list2->val);
+      list1 = list1->next;
+      list2 = list2->next;
+    } else {
+      appendNode(&output, list2->val);
+      list2 = list2->next;
+    }
+
+    if (list2 == NULL && list1 != NULL) {
+      appendNode(&output, list1->val);
+      list1 = list1->next;
+    }
+  }
+
   return output;
 }
 
 int main() {
-  struct ListNode *list1 = (struct ListNode *)malloc(sizeof(struct ListNode));
+  int list1[] = {5, 6};
+  int list2[] = {1, 3, 4};
 
-  addAfterNode(list1, 2);
-  addAfterNode(list1, 3);
+  ListNode *head1 = NULL;
+  ListNode *head2 = NULL;
 
-  printList(list1);
+  // Assign list1 to head1
+  for (int i = 0; i < (int)(sizeof(list1) / sizeof(int)); i++) {
+    appendNode(&head1, list1[i]);
+  }
+
+  // Assign list2 to head2
+  for (int i = 0; i < (int)(sizeof(list2) / sizeof(int)); i++) {
+    appendNode(&head2, list2[i]);
+  }
+
+  printf("[ ");
+  printList(head1);
+  printf("]");
+  printf("\n");
+
+  printf("[ ");
+  printList(head2);
+  printf("]");
+  printf("\n");
+
+  printf("[ ");
+  printList(mergeTwoLists(head1, head2));
+  printf("]");
+  printf("\n");
   return 0;
 }
